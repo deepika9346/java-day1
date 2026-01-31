290d5e3c4f4c5dc46ad4e8bdd18e54399f5c4a66
deepika9346 <deepika77020@gmail.com>
2026-01-31 14:33:19 +0530

    Add files via upload

---
+++ demo2.java
@@ -0,0 +1,10 @@
+class demo2
+{
+    public static void main(String[] args) {
+        {
+            System.out.println("mother thersa institutions");
+            System.out.print("compter science engineering");
+            System.out.println("keerthi");
+        }
+    }
+}
