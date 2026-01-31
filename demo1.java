57c0a0bcdc438991d727991fc6cc3ae33507d39f
deepika9346 <deepika77020@gmail.com>
2026-01-31 14:34:39 +0530

    Add files via upload

---
+++ demo1.java
@@ -0,0 +1,13 @@
+class Demo1
+{
+    public static void main(String[] args)
+    {
+    System.out.println("Hello world");
+    System.out.print("Deepika");
+    System.out.println("Sweety"); 
+    int a=10;
+    System.out.println(a);
+    }
+}
+
+    
